5b0a981a5fa0c7815a62cb58d5cafd358188b333
AnnyXi <1061555@hr.nl>
2023-04-25 15:48:10 +0200

    Cat moves in diferent direction

--- src/js/game.js
+++ src/js/game.js
@@ -11,11 +11,48 @@ export class Game extends Engine {
 
     startGame() {
         console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
-        this.add(fish)
+
+        // const fish = new Actor({
+        //     width: Resources.Fish.width,
+        //     height: Resources.Fish.height
+        //     //this have to be add to be clickable
+        // })
+        // fish.graphics.use(Resources.Fish.toSprite())
+        // fish.pos = new Vector(400, 300)
+        // fish.vel = new Vector(-10, 0)
+        // this.add(fish)
+
+        const background = new Actor({
+            z: -1000,
+        });
+        background.pos = new Vector(400, 300)
+        // the venter of the image position  400 is the half of the window with 
+        background.graphics.use(Resources.Background.toSprite())
+        background.scale = new Vector(1.36, 0.86)
+        this.add(background)
+
+        for (let i = 0; i < 100; i++) {
+            let cat = new Actor({
+                width: Resources.Cat.width,
+                height: Resources.Cat.height
+            })
+            cat.graphics.use(Resources.Cat.toSprite())
+            cat.pos = new Vector(Math.random() * 800, Math.random() * 600)
+            cat.vel = new Vector(Math.random() * 20 -10, Math.random() * 20 -10)
+            //direction and speed how it moves
+            // cat.vel = new Vector(10, 10);
+            cat.scale = new Vector(3.0, 3.0) //scale of the image, (1.0, 1.0) = 100%
+            cat.enableCapturePointer = true // subscribe to input
+
+            cat.pointer.useGraphicsBounds = true;
+            cat.enableCapturePointer = true;
+            cat.on("pointerup", (event) => {
+                cat.kill();
+            });
+
+            this.add(cat)
+
+        }
     }
 }
 
--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,15 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import fishImage from '../images/fish.png'
+import catImage from "../images/cat.png"
+import cowImage from "../images/cow.png"
+import backgroundImage from "../images/background.png"
 
 const Resources = {
-    Fish: new ImageSource(fishImage)
+    Fish: new ImageSource(fishImage),
+    Cat: new ImageSource(catImage),
+    Cow: new ImageSource(cowImage),
+    Background: new ImageSource(backgroundImage)
 }
-const ResourceLoader = new Loader([Resources.Fish])
+const ResourceLoader = new Loader([Resources.Fish, Resources.Cat, Resources.Cow, Resources.Background])
 
 export { Resources, ResourceLoader }
